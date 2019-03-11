234415c67b1682e5c99d3281cd14389f786ad751
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-03-11 09:39:49 -0400

    vue cli generated boilerplate

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>This page is all about {{name}}</h2>
+    <input type="text" class="form-control" />
+  </div>
+</template>
+<style lang="sass">
+
+</style>
+
+<script>
+export default {
+  data: ()=> ({
+    name: "Moshe"
+  }),
+
+}
+</script>
