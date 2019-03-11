c7b7ed7e5989ff944352db055fe17bf2703884ae
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-03-11 11:18:38 -0400

    when the network went down on mar 11

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>This page is all about {{name}}</h2>
-    <input type="text" class="form-control" />
+  <div class="row">
+    <div class="col">
+      <h1>This is {{name}}'s page. He is {{age}} years old </h1>
+      <input v-model="age" type="text" class="form-control" placeholder="Age" />
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" :style="{width: age + '%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
-<style lang="sass">
-
-</style>
 
 <script>
 export default {
-  data: ()=> ({
-    name: "Moshe"
-  }),
-
+    data: ()=> ({
+      name: "Moshe",
+      age: 42
+    })
 }
 </script>
+
+<style type="scss">
+
+</style>
+
